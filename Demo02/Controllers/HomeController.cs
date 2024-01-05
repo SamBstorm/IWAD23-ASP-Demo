2320d24ffb0bb4b1d06e7a4700357bd291388c30
Samuel Legrain <samuel.legrain@bstorm.be>
2024-01-05 14:52:44 +0100

    Ajout Démonstration Vue Part02

--- Demo02/Controllers/HomeController.cs
+++ Demo02/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace Demo02.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Vue1()
+        {
+            return View();
+        }
     }
 }
