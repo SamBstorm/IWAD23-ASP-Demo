02bfb3987333503b58c67d89c298d832332f05be
Samuel Legrain <samuel.legrain@bstorm.be>
2024-01-10 11:08:46 +0100

    Update Exo02 : Formulaire Part02

--- ASP-Exo-Formulaire/Controllers/ProductController.cs
+++ ASP-Exo-Formulaire/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using ASP_Exo_Formulaire.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.RegularExpressions;
 
 namespace ASP_Exo_Formulaire.Controllers
 {
@@ -33,9 +35,39 @@ namespace ASP_Exo_Formulaire.Controllers
         /// <param name="form">Contenu du formulaire</param>
         /// <returns>Redirection vers la vue de Liste si le formulaire est valide</returns>
         [HttpPost]
-        public IActionResult Add(IFormCollection form)
+        public IActionResult Add(ProductAddForm form)
         {
-            return View();
+            if (form is null) ModelState.AddModelError(nameof(form),"Veuillez remplir le formulaire");
+            ValidateProductAddForm(form, ModelState);
+            if(_products.ContainsKey(form.Barcode)) ModelState.AddModelError(nameof(form.Barcode), "Code-barre déjà enregistrer.");
+            if (!ModelState.IsValid) return View();
+            ProductModel product = new ProductModel()
+            {
+                Name = form.Name.Trim(),
+                Barcode = form.Barcode.Trim(),
+                BuyingPrice = form.BuyingPrice,
+                SalePrice = form.SalePrice
+            };
+            _products.Add(form.Barcode, product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private static void ValidateProductAddForm(ProductAddForm form, ModelStateDictionary modelState)
+        {
+            if (string.IsNullOrWhiteSpace(form.Name))
+                modelState.AddModelError(nameof(form.Name), "Le nom du produit est obligatoire");
+            if (!string.IsNullOrWhiteSpace(form.Name) && form.Name.Length > 16)
+                modelState.AddModelError(nameof(form.Name), "Le nom du produit doit contenir au maximum 16 caractères");
+            if (string.IsNullOrWhiteSpace(form.Barcode))
+                modelState.AddModelError(nameof(form.Barcode), "Le code-barre est obligatoire");
+            if (!string.IsNullOrWhiteSpace(form.Barcode) && form.Barcode.Length != 13)
+                modelState.AddModelError(nameof(form.Barcode), "Le code-barre doit être composé de 13 chiffres.");
+            if (!string.IsNullOrWhiteSpace(form.Barcode) && !Regex.IsMatch(form.Barcode,"^[0-9]*$"))
+                modelState.AddModelError(nameof(form.Barcode), "Le code-barre doit être composé que de chiffres.");
+            if (form.BuyingPrice <= 0)
+                modelState.AddModelError(nameof(form.BuyingPrice), "Le prix d'achat doit être supérieur à 0 €.");
+            if (form.SalePrice is not null && form.SalePrice <= form.BuyingPrice)
+                modelState.AddModelError(nameof(form.SalePrice), "Le prix de vente doit être toujours supérieur au prix d'achat.");
         }
     }
 }
---
+++ ASP-Exo-Formulaire/Models/ProductAddForm.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP_Exo_Formulaire.Models
+{
+    public class ProductAddForm
+    {
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public decimal BuyingPrice { get; set; }
+        public decimal? SalePrice { get; set; }
+    }
+}
