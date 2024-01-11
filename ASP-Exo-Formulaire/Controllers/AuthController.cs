6390f09bc1eb3c3518e183492eb66e52092b6d71
Samuel Legrain <samuel.legrain@bstorm.be>
2024-01-11 15:45:59 +0100

    Add Demo : Scaffolding

---
+++ ASP-Exo-Formulaire/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using ASP_Exo_Formulaire.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Exo_Formulaire.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Login));
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(AuthLoginForm form)
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP-Exo-Formulaire/Models/AuthLoginForm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Exo_Formulaire.Models
+{
+    public class AuthLoginForm
+    {
+        [DisplayName("Votre adresse e-mail :")]
+        [Required(ErrorMessage = "L'adresse e-mail est nécessaire pour se connecter.")]
+        [MinLength(5, ErrorMessage ="Une adresse est toujours composé de minimum 5 caractères")]
+        [MaxLength(256, ErrorMessage ="Une adresse est toujours composé de maximum 256 caractères")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [DisplayName("Votre mot de passe :")]
+        [Required(ErrorMessage = "Le mot de passe est nécessaire pour se connecter.")]
+        [MinLength(8, ErrorMessage = "Un mot de passe efficace est composé de minimum 8 caractères")]
+        [MaxLength(16, ErrorMessage = "Un mot de passe est composé de maximum 16 caractères")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
