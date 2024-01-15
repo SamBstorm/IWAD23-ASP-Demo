e38315ce7e22e4be314699a55eb1fc1716ab3184
Samuel Legrain <samuel.legrain@bstorm.be>
2024-01-15 16:16:50 +0100

    Add Demo-DAL+BLL Part01

---
+++ DAL-Demo-DBSlide/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL_Demo_DBSlide.Entities
+{
+    public class Student
+    {
+        public int Student_Id { get; set; }
+        public string? First_name { get; set; }
+        public string? Last_name { get; set; }
+        public DateTime? Birth_date { get; set; }
+        public string? Login { get; set; }
+        public int? Section_id { get; set; }
+        public int? Year_result { get; set; }
+        public string Course_id { get; set; }
+    }
+}
---
+++ DAL-Demo-DBSlide/Services/StudentService.cs
@@ -0,0 +1,16 @@
+﻿using DAL_Demo_DBSlide.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL_Demo_DBSlide.Services
+{
+    public class StudentService
+    {
+        private string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBSlide;Integrated Security=True;Encrypt=True";
+        public IEnumerable<Student> Get()
+        {
+
+        }
+    }
+}
