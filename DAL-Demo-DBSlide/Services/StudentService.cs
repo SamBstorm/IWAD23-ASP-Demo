fca7b451d15e6dca2c0b99302b4c7cf8a8a587f2
Samuel Legrain <samuel.legrain@bstorm.be>
2024-01-16 12:04:21 +0100

    Update Demo-DAL+BLL part03

--- DAL-Demo-DBSlide/Services/StudentService.cs
+++ DAL-Demo-DBSlide/Services/StudentService.cs
@@ -2,6 +2,7 @@
 using DAL_Demo_DBSlide.Mappers;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
@@ -25,6 +26,81 @@ namespace DAL_Demo_DBSlide.Services
                             yield return reader.ToStudent();
                         }
                     }
+                }
+            }
+        }
+
+        public Student Get(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM [student] WHERE [student_id] = @id";
+                    /* Paramètre Méthode difficile
+                    SqlParameter param_01 = new SqlParameter();
+                    param_01.ParameterName = "id";
+                    param_01.Value = id;
+                    command.Parameters.Add(param_01);*/
+                    command.Parameters.AddWithValue("id", id);
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read()) return reader.ToStudent();
+                        throw new ArgumentException(nameof(id), $"L'identifiant {id} n'existe pas dans la base de données.");
+                    }
+                }
+            }
+        }
+
+        public int Insert(Student data)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_student_insert";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("first_name", data.First_name);
+                    command.Parameters.AddWithValue("last_name", data.Last_name);
+                    command.Parameters.AddWithValue("birth_date", data.Birth_date);
+                    command.Parameters.AddWithValue("section_id", data.Section_id);
+                    command.Parameters.AddWithValue("year_result", data.Year_result ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("course_id", data.Course_id);
+                    connection.Open();
+                    return (int) command.ExecuteScalar();
+                }
+            }
+        }
+
+        public void Update(Student data) {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_student_update_year_result";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("id", data.Student_Id);
+                    command.Parameters.AddWithValue("year_result", data.Year_result ?? (object)DBNull.Value);
+                    connection.Open();
+                    if (command.ExecuteNonQuery() <= 0)
+                        throw new ArgumentException(nameof(data.Student_Id), $"L'identifiant {data.Student_Id} n'est pas das la base de données");
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_student_delete";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("id", id);
+                    connection.Open();
+                    if (command.ExecuteNonQuery() <= 0)
+                        throw new ArgumentException(nameof(id), $"L'identifiant {id} n'est pas das la base de données");
                 }
             }
         }
